17557bc6fabcc9b4d669e4cc0baf347998e74ed7
gusoliveira21 <42920754+gusoliveira21@users.noreply.github.com>
2022-02-09 18:30:47 -0300

    Aperfeiçoa método de inserção de ponto
    Agora, entre cada expressão matemática (que é dividida por símbolos matemáticos como soma, subtração,..) é somente permitido o uso de ponto (simbolizando virgulas), somente uma vez.

--- Calculadora/app/src/main/java/gusoliveira21/calculadora/CalculatorViewModel.kt
+++ Calculadora/app/src/main/java/gusoliveira21/calculadora/CalculatorViewModel.kt
@@ -6,10 +6,8 @@ import androidx.lifecycle.ViewModel
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class CalculatorViewModel : ViewModel() {
-    //TODO: Depois de uma expressão cujo o resultado é um valor co virgula, aoefetuar outra conta o app buga
-    // Realizar uma modificação para substituir virgula por ponto sempre que aparecer.
+
     private val _listaDeSimbolos = listOf("+", "-", "*", "/", ".")
-    private val _listaDeValores = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
 
     private val _expressaoDigitada = MutableLiveData("")
     val expressaoDigitada: LiveData<String>
@@ -22,7 +20,7 @@ class CalculatorViewModel : ViewModel() {
         _expressaoDigitada.value = _expressaoDigitada.value + valor
     }
 
-    fun digitaSimbolMath(valor: Int) {
+    fun onAddSimbolMath(valor: Int) {
         if (valor in 0..9) onDigitaExpressao("$valor")
         else if (onConditionalTest(expressaoDigitada)) {
             when (valor) {
@@ -30,19 +28,18 @@ class CalculatorViewModel : ViewModel() {
                 12 -> onDigitaExpressao("-")
                 13 -> onDigitaExpressao("*")
                 14 -> onDigitaExpressao("/")
-                15 -> if (onVerificaSeJaFoiDigitadoUmPontoAnteriormente(expressaoDigitada) == false) onDigitaExpressao(".")
+                15 -> if (onCheckIfAHasDot(expressaoDigitada) == true) onDigitaExpressao(
+                    ".")
             }
         }
     }
 
-    fun onConditionalTest(expressaoDigitada: LiveData<String>): Boolean =
-        ((onHasElementList(expressaoDigitada) && !onCheckIfLastElementOfListIsSymbol(expressaoDigitada)))
+    private fun onConditionalTest(expressaoDigitada: LiveData<String>): Boolean =
+        ((onHasElementList(expressaoDigitada) && !onCheckIfLastElementOfListIsSymbol(
+            expressaoDigitada)))
 
     private fun onHasElementList(campoDigitado: LiveData<String>): Boolean =
-        ((campoDigitado.value!!.length).toString() != "0") //(=0)vazio/(!=0)com elementos
-
-
-
+        ((campoDigitado.value!!.length).toString() != "0")
 
     //TODO:Se for true, chame a função onDelete ultimo elemento!
     private fun onCheckIfLastElementOfListIsSymbol(campoDigitado: LiveData<String>): Boolean {
@@ -53,45 +50,75 @@ class CalculatorViewModel : ViewModel() {
         }
     }
 
-
     //TODO: Se já houver um simbolo, chamar função para apagar o mesmo e digitar o novo simbolo no lugar
     private fun onCheckIfIsASymbol(elementoParaVerificarSeESimbolo: String): Boolean =
         ((_listaDeSimbolos.indexOf(elementoParaVerificarSeESimbolo)) != -1)
-    //true = Sim,  possivel escrever o simbolo na tela
-    //false = Não, não é possivel escrever o simbolo na tela, já tem um simbolo no local
 
+    private fun onCheckIfAHasDot(campoDigitado: LiveData<String>): Boolean {
+        var qtdElementos = campoDigitado.value!!.length
+        if(contSimbol(qtdElementos,campoDigitado) == 0)
+            return true
+        else if(contDot(qtdElementos,campoDigitado) == 0)
+            return true
+        else if(findSimbol(qtdElementos,campoDigitado))
+            return true
+        return false
+    }
 
-
-    //TODO: Permite que digitemos no formato "00.00", não podemos digitar "0.0.0.0.0"
-    fun onVerificaSeJaFoiDigitadoUmPontoAnteriormente(campoDigitado: LiveData<String>): Boolean {
-        for (i in campoDigitado.value!!.length - 2 downTo 0 step 1) {
-            if (campoDigitado.value!![i].toString().equals(_listaDeSimbolos[4])) return true
-
-            _listaDeSimbolos.forEachIndexed { index, value ->
-                if (campoDigitado.value!![i].toString().equals("$value")) return false
-            }
-
-            _listaDeValores.forEachIndexed { index, value ->
-                if (campoDigitado.value!![i].toString().equals("$value"))
-                    _listaDeSimbolos.forEachIndexed { index, value ->
-                        if (campoDigitado.value!![i - 1].toString().equals("$value")) return false
-                    }
-            }
+    private fun findSimbol(qtdElementos:Int, campoDigitado: LiveData<String>):Boolean{
+        for (i in qtdElementos - 1 downTo 0 step 1){
+            if (campoDigitado.value!![i].toString() == _listaDeSimbolos[0] ||
+                campoDigitado.value!![i].toString() == _listaDeSimbolos[1] ||
+                campoDigitado.value!![i].toString() == _listaDeSimbolos[2] ||
+                campoDigitado.value!![i].toString() == _listaDeSimbolos[3])
+                    return true
+            else if(campoDigitado.value!![i].toString() == _listaDeSimbolos[4])
+                return false
         }
         return false
     }
+
+    private fun contDot(qtdElementos:Int, campoDigitado: LiveData<String>):Int{
+        var contDot = 0
+        for (i in qtdElementos - 1 downTo 0 step 1)
+                if (campoDigitado.value!![i].toString() == _listaDeSimbolos[4])
+                    contDot+=1
+        return contDot
+    }
+
+    private fun contSimbol(qtdElementos:Int, campoDigitado: LiveData<String>):Int{
+        var simbol = 0
+        for (i in qtdElementos - 1 downTo 0 step 1)
+            _listaDeSimbolos.forEachIndexed { index, value ->
+                if (campoDigitado.value!![i].toString() == value)
+                    simbol+=1
+            }
+        return simbol
+    }
+
 
     fun onDeleteAll() {
         _expressaoDigitada.value = clean
     }
 
     fun onDeleteTheLastElement() {
-        _expressaoDigitada.value = (_expressaoDigitada.value!!.subSequence(0,
-            _expressaoDigitada.value!!.length - 1)).toString()
+        if (onHasElementList(_expressaoDigitada) == true)
+            _expressaoDigitada.value = (_expressaoDigitada.value!!.subSequence(0,
+                _expressaoDigitada.value!!.length - 1)).toString()
     }
 
     fun result() {
-        _expressaoDigitada.value = onCalculaResult(expressaoDigitada)
+        _expressaoDigitada.value = retiraVirgula(onCalculaResult(expressaoDigitada))
+    }
+
+    private fun retiraVirgula(onCalculaResult: String): String? {
+        var valor = ""
+        onDeleteAll()
+        onCalculaResult.forEachIndexed { index, value ->
+            if ((onCalculaResult[index].toString()) == ",") valor = "$valor."
+            else valor += value
+        }
+        return valor
     }
 
     private fun onCalculaResult(expressaoDigitada: LiveData<String>): String {
